656c2e26a5e164633b1e007116c06b97ef01b8af
petterhg <petter@growbotics.se>
2025-01-08 18:44:15 +0100

    First commit

---
+++ tests/test_adapter.py
@@ -0,0 +1,30 @@
+import pytest
+
+from mangum.exceptions import ConfigurationError
+from mangum import Mangum
+
+
+async def app(scope, receive, send):
+    ...
+
+
+def test_default_settings():
+    handler = Mangum(app)
+    assert handler.lifespan == "auto"
+    assert handler.api_gateway_base_path == "/"
+
+
+@pytest.mark.parametrize(
+    "arguments,message",
+    [
+        (
+            {"lifespan": "unknown"},
+            "Invalid argument supplied for `lifespan`. Choices are: auto|on|off",
+        ),
+    ],
+)
+def test_invalid_options(arguments, message):
+    with pytest.raises(ConfigurationError) as exc:
+        Mangum(app, **arguments)
+
+    assert str(exc.value) == message
